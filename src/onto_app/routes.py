8330c37a9b2a8895ac7ff2aa4738cd2b73dc4d27
Anirudha123 <anirudha.ramesh@students.iit.ac.in>
2018-10-07 16:39:37 +0530

    Added documentation

--- src/onto_app/routes.py
+++ src/onto_app/routes.py
@@ -7,13 +7,23 @@ from werkzeug.utils import secure_filename
 import json
 from onto_app.onto import *
 
+# prevent cached responses
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "-1"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+""" Loads ontology to database """ 
 @app.route('/', methods=["GET", "POST"])
 def hello():
     if request.method == 'GET' :
         add_onto_file(1, "pizza", "./data/owl/pizza.owl", "./data/json/pizza.json", "./data/new/pizza.txt")
-        return "Pizza ontology has been added"
+        return "Pizza ontology has been added to database"
     if request.method == 'POST' :
-    
+        """ Remanants of testing code, will be removed later when they will be no longer be used with certainity. """ 
         """ Returning name and type of links. Does not update with objects. Bias for new yet to be set, will be done so when more of the backend for it is built. """
         a = str(request.data).split(',')    
         Prop = a[0]
@@ -41,12 +51,14 @@ def hello():
 
     return render_template("index.html")
 
+""" Stores decisions taken in frontend corresponding to relationships accept/reject into database """ 
 @app.route('/decision', methods=["POST"])
 def decision() : 
     if request.method == 'POST' : 
         """ Decisions stored """
+        """ Index numbers used to extract specific content from already existing inner html. This will hold through across cases.""" 
         data = str(request.data).split(',')
-        print(data)
+        # print(data)
         Prop = data[0][11:-1]
         Type = data[4][8:-1]
         Decision  = data[1][12:-1]
@@ -60,72 +72,29 @@ def decision() :
         print("Type : ", Type)
         user_id = 1
         onto_id = 1
+        """ Call add_decision from onto.py to store decision """ 
         add_decision(user_id, Prop, Domain, Range, Type, onto_id, {'Accept': 1, 'Reject':0}[Decision])
 
     return render_template("index.html")
 
 
-# prevent cached responses
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "-1"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
-@app.route('/return-files/<path:filename>/', methods = ['GET', 'POST'])
-def return_files(filename):
-    print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-    try:
-        print("######################################")
-        uploads = os.path.join(current_app.root_path, "OntoData") # change with a app.config thing
-        print(uploads)
-        
-        # f = open(uploads + '/' + filename + '1' , 'w')
-        # f.write(a)
-        # print(repr(a))
-        # f.close()
-        return send_from_directory(uploads, filename,as_attachment=True, attachment_filename=filename)
-    except Exception as e:
-        return str(e)
-
-    # @app.route('/uploadfile/')
-    # def upload_files(filename) :
-    #     try :
-    # @app.route('/uploadFile/', methods=['GET', 'POST'])
-    # def upload_file():
-    #     if request.method == 'POST':
-    #         # check if the post request has the file part
-    #         if 'file' not in request.files:
-    #             flash('No file part')
-    #             return redirect(request.url)
-    #         file = request.files['file']
-    #         # if user does not select file, browser also
-    #         # submit an empty part without filename
-    #         if file.filename == '':
-    #             flash('No selected file')
-    #             return redirect(request.url)
-    #         if file:
-    #             filename = secure_filename(file.filename)
-    #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    #             return redirect(url_for('uploaded_file',
-    #                                     filename=filename))
-
-@app.route("/loadOntology/<path:filename>/", methods = ['GET', 'POST'])
+""" Serve file and new relationships from backend corresponding to the filename given in the URL """
+@app.route("/loadOntology/<path:filename>/", methods = ['GET'])
 def loadOntology(filename) : 
+    """ Serve files and new relations from the backend """ 
+    """ Ontologies ready to be rendered saved in data/json """
+  
     uploads = os.path.join(current_app.root_path,"data/json")
     uploads = uploads + "/" + str(filename) 
 
     fname = str(filename) 
     fname = fname.split(".")[0]
     fname = fname + ".txt"
+
+    """ Corresponding new relations for given ontology are stored in data/new. """
+
     new_relations = get_new_relations(os.path.join(current_app.root_path,"data/new")+ "/" + fname)
-    print(new_relations)
+    # print(new_relations)
 
     try :
         with open(uploads,"r") as json_data:
@@ -135,16 +104,43 @@ def loadOntology(filename) :
         flash('Oops record not found')
         return redirect(url_for('hello'))
 
-    # try :
-    #     with open(new_relations[:-1],"r") as json_data:
-    #         NewRelcontents = json.load(json_data)
-    #         print(NewRelcontents)
-    # except :
-    #     flash('Oops new rel text not found')
-    #     return redirect(url_for('hello'))
-
-    # contents = f.read()
-
-    # print(contents)
-    #return redirect(url_for('hello'))
     return render_template("index.html", OntologyContentJson = contents, hiddenJSON = new_relations)
+    
+
+# @app.route('/return-files/<path:filename>/', methods = ['GET', 'POST'])
+# def return_files(filename):
+#     print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+#     try:
+#         print("######################################")
+#         uploads = os.path.join(current_app.root_path, "OntoData") # change with a app.config thing
+#         print(uploads)
+        
+#         # f = open(uploads + '/' + filename + '1' , 'w')
+#         # f.write(a)
+#         # print(repr(a))
+#         # f.close()
+#         return send_from_directory(uploads, filename,as_attachment=True, attachment_filename=filename)
+#     except Exception as e:
+#         return str(e)
+
+#     # @app.route('/uploadfile/')
+#     # def upload_files(filename) :
+#     #     try :
+#     # @app.route('/uploadFile/', methods=['GET', 'POST'])
+#     # def upload_file():
+#     #     if request.method == 'POST':
+#     #         # check if the post request has the file part
+#     #         if 'file' not in request.files:
+#     #             flash('No file part')
+#     #             return redirect(request.url)
+#     #         file = request.files['file']
+#     #         # if user does not select file, browser also
+#     #         # submit an empty part without filename
+#     #         if file.filename == '':
+#     #             flash('No selected file')
+#     #             return redirect(request.url)
+#     #         if file:
+#     #             filename = secure_filename(file.filename)
+#     #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#     #             return redirect(url_for('uploaded_file',
+#     #                                     filename=filename))
